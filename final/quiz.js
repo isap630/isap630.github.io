c0d7afac84f2c54e6b1205458f8aaf02048985d6
Arlenis Pena <101661696+isap630@users.noreply.github.com>
2024-05-13 10:29:06 -0400

    Add files via upload

--- final/quiz.js
+++ final/quiz.js
@@ -4,26 +4,26 @@ let num = 0;
 
 //page1
 function mouseOverp1_B1FUNC(){
-    document.getElementById("status-icon").src = "my_icons/motivated.png";
+    document.getElementById("status-icon").src = "my_icons/motivated.PNG";
 }
 function mouseOverp1_B2FUNC(){
-    document.getElementById("status-icon").src = "my_icons/relaxed.png";
+    document.getElementById("status-icon").src = "my_icons/relaxed.PNG";
 }
 
 function mouseOutp1FUNC(){
-	document.getElementById("status-icon").src = "my_icons/thinking.png";
+	document.getElementById("status-icon").src = "my_icons/thinking.PNG";
 }
 
 //page2 
 function mouseOverp2_B1FUNC(){
-    document.getElementById("status-icon").src = "my_icons/indoors.png";
+    document.getElementById("status-icon").src = "my_icons/indoors.PNG";
 }
 function mouseOverp2_B2FUNC(){
-    document.getElementById("status-icon").src = "my_icons/outdoors.png";
+    document.getElementById("status-icon").src = "my_icons/outdoors.PNG";
 }
 
 function mouseOutp2FUNC(){
-	document.getElementById("status-icon").src = "my_icons/original.png";
+	document.getElementById("status-icon").src = "my_icons/original.PNG";
 }
 
 //page2 
