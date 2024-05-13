d5e0fa8b3d8f4ae1221ebe67f0e8ff5fd89a2cad
Arlenis Pena <101661696+isap630@users.noreply.github.com>
2024-05-13 10:12:06 -0400

    Add files via upload

---
+++ final/quiz.js
@@ -0,0 +1,41 @@
+
+let page = 1;
+let num = 0;
+
+//page1
+function mouseOverp1_B1FUNC(){
+    document.getElementById("status-icon").src = "my_icons/motivated.png";
+}
+function mouseOverp1_B2FUNC(){
+    document.getElementById("status-icon").src = "my_icons/relaxed.png";
+}
+
+function mouseOutp1FUNC(){
+	document.getElementById("status-icon").src = "my_icons/thinking.png";
+}
+
+//page2 
+function mouseOverp2_B1FUNC(){
+    document.getElementById("status-icon").src = "my_icons/indoors.png";
+}
+function mouseOverp2_B2FUNC(){
+    document.getElementById("status-icon").src = "my_icons/outdoors.png";
+}
+
+function mouseOutp2FUNC(){
+	document.getElementById("status-icon").src = "my_icons/original.png";
+}
+
+//page2 
+function mouseOverp3_B1FUNC(){
+    document.getElementById("status-icon").src = "my_icons/productive.png";
+}
+function mouseOverp3_B2FUNC(){
+    document.getElementById("status-icon").src = "my_icons/fun.png";
+}
+
+function mouseOutp3FUNC(){
+	document.getElementById("status-icon").src = "my_icons/original.png";
+}
+
+
